c775417d0c8946509333656743118bca43f5a8af
zoltan-gacsal <gacsalzoltan2017@gmail.com>
2023-11-04 16:23:08 +0100

    Navbar made with bootstrap template

--- news_media/src/App.js
+++ news_media/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import axios from 'axios';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   
-
+ return(
+  <div>
+    
+    <Navbar />
+    
+  </div>
+ )
+  
             
 
 }
