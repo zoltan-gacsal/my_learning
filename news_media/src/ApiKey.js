0888539a79ec72fefd5f0e0c2c3fe8e63acda159
zoltan-gacsal <gacsalzoltan2017@gmail.com>
2023-11-04 16:52:38 +0100

    Create the apiKey component

---
+++ news_media/src/ApiKey.js
@@ -0,0 +1,5 @@
+const Api_Key = {
+    apiKey: '99af0a57ff23425e86456c6820990490'
+};
+
+export default Api_Key;
---
+++ news_media/src/Components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+
+const Main = () => {
+
+    const [hir, setHir] = useState([]);
+
+    
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Main
